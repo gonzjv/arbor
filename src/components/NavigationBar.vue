aca3d40bae6c022939eb1f1ad9693380cd3f4605
gonzjv <gonzjv@ya.ru>
2022-09-16 15:06:20 +0300

    build: add tailwindcss & feat: add header with logo

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,25 @@
+<script setup>
+import logoImg from './../assets/logo.svg';
+
+const navBtns = [
+  'platfrom',
+  'advantages',
+  'ecosystem',
+];
+</script>
+
+<template>
+  <header class="flex gap-3 min-w-full h-20">
+    <!-- <div>Logo</div> -->
+    <img
+      :src="logoImg"
+      class=""
+      alt="Arbor logo"
+    />
+    <nav class="">
+      <ul class="flex gap-5">
+        <li v-for="btn in navBtns">{{ btn }}</li>
+      </ul>
+    </nav>
+  </header>
+</template>
