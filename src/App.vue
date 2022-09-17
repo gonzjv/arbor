b95543d14da602c07bc95214e0f2f49fb3ee0fe1
gonzjv <gonzjv@ya.ru>
2022-09-17 23:35:35 +0300

    feat: about text and button

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import NavigationBar from './components/NavigationBar.vue';
+import AboutPlatform from './components/AboutPlatform.vue';
 </script>
 
 <template>
   <div class="md:w-[1160px]">
     <NavigationBar />
+    <AboutPlatform />
   </div>
 </template>
--- src/components/AboutPlatform.vue
+++ src/components/AboutPlatform.vue
@@ -1,4 +1,21 @@
 <script setup></script>
 <template>
-  <main>about</main>
+  <main class="h-[680px] relative">
+    <article
+      class="absolute bottom-32 left-0 flex flex-col items-start gap-3"
+    >
+      <h2 class="text-6xl">ARBOR PLATFORM</h2>
+      <p class="w-96 text-left text-lg">
+        {{ $t('aboutPlatform.text') }}
+      </p>
+    </article>
+    <div
+      class="absolute bottom-0 left-0 w-72 h-20 border border-green-400 rounded-full blur-sm"
+    ></div>
+    <button
+      class="text-2xl text-slate-700 absolute bottom-2 left-4 w-64 h-16 bg-green-400 rounded-full blur-none"
+    >
+      {{ $t('aboutPlatform.try') }}
+    </button>
+  </main>
 </template>
