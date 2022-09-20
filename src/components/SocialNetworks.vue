bca9fa0a5179aa8b4235404c329f21ca08c7ea19
gonzjv <gonzjv@ya.ru>
2022-09-20 10:04:59 +0300

    refactor: social networks links edit

--- src/components/SocialNetworks.vue
+++ src/components/SocialNetworks.vue
@@ -7,18 +7,21 @@ const SOCIAL_NETWORKS = [
   {
     title: 'VK',
     img: logoVk,
+    link: 'https://vk.com/arborplatform',
     style:
       'flex justify-center items-center w-1/3 bg-green-400 hover:bg-green-800',
   },
   {
     title: 'Github',
     img: logoGithub,
+    link: 'https://github.com/Arbor-platform',
     style:
       'flex justify-center items-center w-1/3 bg-black hover:bg-white',
   },
   {
     title: 'Medium',
     img: logoMedium,
+    link: 'https://github.com/Arbor-platform',
     style:
       'flex justify-center items-center w-1/3 bg-green-800 hover:bg-green-400',
   },
@@ -26,14 +29,15 @@ const SOCIAL_NETWORKS = [
 </script>
 <template>
   <section class="flex h-[480px]">
-    <figure
+    <a
       v-for="network in SOCIAL_NETWORKS"
       :class="network.style"
+      :href="network.link"
     >
       <img
         :src="network.img"
         :alt="network.title"
       />
-    </figure>
+    </a>
   </section>
 </template>
