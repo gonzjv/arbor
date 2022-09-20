25b628a39bc380e40abc1aa33a817c75ff483187
gonzjv <gonzjv@ya.ru>
2022-09-20 06:45:10 +0300

    refactor:  section

--- src/components/Network.vue
+++ src/components/Network.vue
@@ -2,9 +2,13 @@
 import networkImg from '@/assets/network.svg';
 </script>
 <template>
-  <figure>
+  <figure
+    class="flex gap-[140px] px-[140px] py-36"
+  >
     <img :src="networkImg" alt="Network" />
-    <figcaption class="flex flex-col gap-5">
+    <figcaption
+      class="flex flex-col gap-5 w-full justify-center text-left text-3xl font-semibold text-green-400"
+    >
       <p>
         {{ $t('network.arbor') }}
       </p>
